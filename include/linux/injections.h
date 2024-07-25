cbd756ce02817908002d7bc52d4bba039556727e
Tanya Prasad <tanyapsd@students.cs.ubc.ca>
2024-07-25 13:58:56 -0700

    added injections.h, final version

---
+++ include/linux/injections.h
@@ -0,0 +1,48 @@
+#ifndef INJECTIONS_H
+#define INJECTIONS_H
+
+typedef void (*injected_func_type)();
+extern injected_func_type pointers[100];
+extern void kernel_noop(void);
+extern void set_pointer(int i, void (*f)());
+
+enum memtrace_state_lags {
+	TAPE_OPS = 1 << 0,
+	SWAP_SSD_OPTIMIZATION = 1 << 1,
+	FASTSWAP_ASYNCWRITES = 1 << 2,
+
+	TAPE_FETCH = 1 << 3,
+	LRU_LOGS = 1 << 4,
+
+	OFFLOAD_FETCH = 1 << 5,
+	MARK_UNEVICTABLE = 1 << 6,
+
+	ONE_TAPE = 1 << 7,
+};
+
+// space for mem_pattern_trace state which needs to live accross injector
+// module reloads.
+typedef struct {
+	int flags;
+	// in case I need to add more fields before doing a kernel recompile
+	char padding[60];
+} mem_pattern_trace_state;
+
+extern mem_pattern_trace_state memtrace_state;
+
+static inline void memtrace_setflag(int flag)
+{
+	memtrace_state.flags |= flag;
+}
+
+static inline int memtrace_getflag(int flag)
+{
+	return memtrace_state.flags & flag;
+}
+
+static inline void memtrace_clearflag(int flag)
+{
+	memtrace_state.flags &= ~flag;
+}
+
+#endif /* INJECTIONS_H */
