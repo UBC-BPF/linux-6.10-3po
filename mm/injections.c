05caf84991e85157f111de6dab00a733fe728a02
Tanya Prasad <tanyapsd@students.cs.ubc.ca>
2024-07-25 14:49:11 -0700

    updated with final version of file

---
+++ mm/injections.c
@@ -0,0 +1,22 @@
+#include <linux/kernel.h>
+#include <linux/export.h>
+#include <linux/injections.h>
+
+void kernel_noop(void)
+{
+}
+injected_func_type pointers[100] = {[0 ... 99] = kernel_noop };
+
+void set_pointer(int i, injected_func_type f)
+{
+	pointers[i] = f;
+}
+EXPORT_SYMBOL(set_pointer);
+EXPORT_SYMBOL(kernel_noop);
+
+mem_pattern_trace_state memtrace_state = {
+	.flags = (TAPE_OPS | SWAP_SSD_OPTIMIZATION | FASTSWAP_ASYNCWRITES |
+		  TAPE_FETCH)
+};
+
+EXPORT_SYMBOL(memtrace_state);
