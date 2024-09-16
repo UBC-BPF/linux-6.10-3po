159a7f47d8fb12dee6ed17aa06b6d59a5050cf8c
Tanya Prasad <tanyapsd@students.cs.ubc.ca>
2024-09-16 01:23:57 -0700

    Use SYSCALL_DEFINEx instead of asmlinkage to define new syscall

--- tracer/tracer.c
+++ tracer/tracer.c
@@ -1,10 +1,11 @@
 #include "tracer.h"
 #include <linux/printk.h>
 #include<linux/injections.h>
+#include <linux/syscalls.h>
 
-asmlinkage int sys_mem_pattern_trace(int flags) {
+SYSCALL_DEFINE1(mem_pattern_trace, int, flags)
+{
 	printk (KERN_INFO "in mem pattern trace start %d", flags);
 	(*pointers[3])(flags);
 	return 0;
 }
-
